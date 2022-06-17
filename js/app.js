8e456b09ef0eccaa65015ee1c2845a702cf76d7c
unitop <support@unitop.vn>
2022-06-17 15:46:02 +0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#eye').click(function(){
+        $(this).toggleClass('open');
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if($(this).hasClass('open')){
+            $(this).prev().attr('type', 'text');
+        }else{
+            $(this).prev().attr('type', 'password');
+        }
+    });
+});
